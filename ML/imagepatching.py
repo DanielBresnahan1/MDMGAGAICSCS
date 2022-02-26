519e082bfe5921819abcfdd5b97aa6afcf54fe4a
DanielBresnahan1 <90290413+DanielBresnahan1@users.noreply.github.com>
2022-02-26 13:19:04 -0600

    Created python class 'Image Patcher' that can create an Image Patcher object, which will patch the images. Either by tiling them, in the case of negative samples, or striding along the lesion, in the case of positive samples (#16)
    Co-authored-by: DanielBresnahan1 <danielbresnahan@unomaha,edu>

---
+++ ML/imagepatching.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import PIL
+import math
+import random
+import os
+import re
+
+class ImagePatcher:
+    
+    def __init__(self, saveDir: str, patchSize: tuple, majorAxisDif=16,
+                 stride=10, imageSize=(6000, 4000)):
+        """
+        
+
+        Parameters
+        ----------
+        saveDir : str
+            Location to save images
+        patchSize : tuple
+            The size of the dimensions of the patch.
+            (width, height)
+        majorAxisDif : int, optional
+            The random distance from the major axis of the lesion to generate 
+            the patch from.
+            ie. size 15 means that the center of the patch will be a random 
+            amount within 15 pixels of the lesion
+        rotBoosting : Boolean, optional
+            IF a random rotation should be applied to boost the total number 
+            of patches from a single image. The default is True.
+        stride : int, optional
+            how far to stride along the major axis of the legion. The default is 10.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.majorAxisDif = majorAxisDif
+        self.saveDir = saveDir
+        self.patchSize = patchSize
+        self.rotBoosting = True
+        self.stride = stride
+        self.imageSize = imageSize
+        self.Folder = "Positive"
+        
+    def patch_path(self, x1, y1, x2, y2):
+        """
+        This generates a list of coordinates, that represent the location on each
+        image, in which a path should be generated. should only be called on 
+        images in which a lesion exists.
+
+        Parameters
+        ----------
+        x1 : int
+            Lesion x1
+        y1 : int
+            Lesion y1
+        x2 : int
+            Lesion x2
+        y2 : int
+            Lesion y2
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        slope = (y2-y1)/(x2-x1)
+        
+        coords = []
+        
+        startx = 0
+        starty = 0
+        
+        if x1<x2:
+            startx=x1
+            endx=x2
+        else:
+            startx=x2
+            endx=x1
+        
+        if y1<y2:
+            starty=y1
+        else:
+            starty=y2
+        
+        yshift=0
+        for x in range(startx, endx, self.stride):
+            yshift += self.stride*slope
+            coords.append((x, starty+yshift))
+        
+        return coords
+    
+    
+    def patch_grid(self):
+        """
+        Generate a list of coordinates, that represents the center of each patch.
+        Used for Negative pictures, such that a grid of the entire image is generated. 
+
+        Returns
+        -------
+        None.
+
+        """
+        difx = self.imageSize[0] % self.patchSize[0]
+        dify = self.imageSize[1] % self.patchSize[1]
+        
+        coords = []
+        
+        for x in range(0, self.imageSize[0]-difx-self.patchSize[0], self.patchSize[0]):
+            for y in range(0, self.imageSize[1]-dify-self.patchSize[1], self.patchSize[1]):
+                coords.append((x+self.patchSize[0]/2, y+self.patchSize[1]/2))
+                
+        return coords
+    
+    
+    def patch(self, imagePath: str, lessionCoords: tuple):
+        """
+        Will generate a number of patches from an image, if the image contains
+        a lession, it will stride along the major axis of the NLCB and create patches.
+        If the image is negative, it will instead break up the images into a grid.
+        
+
+        Parameters
+        ----------
+        imagePath : String
+            The path to save an image to
+        lessionCoords : Tuple
+            Tuples that contains the coordinates for the lession, of the form
+            (x1, y1, x2, y2)
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        #determine weather or not a lession exists, if 0, there is no lession
+        lessionSum = sum(lessionCoords)
+        
+        if lessionSum:
+            self.Folder = "Positive"
+            patch_centers = self.patch_path(*lessionCoords)
+        else:
+            self.Folder = "Negative"
+            patch_centers = self.patch_grid()
+            
+        print(patch_centers)
+        
+        im = PIL.Image.open(imagePath)
+        
+        image_name = re.findall("^(.*?)\.jpg", os.path.basename(imagePath))[0]
+        
+        for x, y in patch_centers:
+            
+            x = random.randint(-1 * self.majorAxisDif/2, self.majorAxisDif/2)
+            y = random.randint(-1 * self.majorAxisDif/2, self.majorAxisDif/2)
+            
+            # radians = angle * math.pi / 180
+            
+            # print("X:{},Y:{}".format(x, y))
+            
+            # rot_x = (x - self.imageSize[0]/2)*math.cos(angle) - (y - self.imageSize[1]/2)*math.sin(angle) + self.imageSize[0]/2
+            # rot_y = (x - self.imageSize[0]/2)*math.sin(angle) - (y - self.imageSize[1]/2)*math.cos(angle) + self.imageSize[1]/2
+            
+            # print("rot_X:{},rot_Y:{}".format(rot_x, rot_y))
+            
+            crop = (x - self.patchSize[0]/2, y - self.patchSize[1]/2,
+                                    x + self.patchSize[0]/2, y +  self.patchSize[1]/2)
+        
+            if all(coord > 0 for coord in crop):
+                im_crop = im.crop(crop)
+                im_crop.save(os.path.join(self.saveDir, self.Folder, 
+                                           "{}_{}_{}.jpg".format(image_name, crop[0], crop[1])))
+        
+
+
+if __name__=="__main__":
+    curDir = os.getcwd()
+    image = "J_170823_134140.jpg"
+    image_dir = os.path.join(curDir, image)
+    
+    patcher = ImagePatcher(os.path.join(curDir, "save"), (224, 224), imageSize=(6000, 4000))
+    patcher.patch(image_dir, (1795,415,1518,0))
+        
+        
+        
+    
+    
+        
+    
+        
+    
+        
