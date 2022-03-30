9782aee55149de883ccf7caa8c0d273725f91ea8
DanielBresnahan1 <90290413+DanielBresnahan1@users.noreply.github.com>
2022-03-30 15:04:07 -0500

    Feature/ct9 professional (#28)
    * This covers severl tasks, and includes updates to Models A, and B. It also creates the script to perform hard negative mining using ModelT_Classify. This commit alos covers the training for networks A and B. However, those files are ignored by GIT
    
    * Made final changes to train Network C, primarily the if__name__
    
    * Added logs to gitignore
    
    * Removed log files from tracing
    
    Co-authored-by: DanielBresnahan1 <danielbresnahan@unomaha,edu>

--- ML/ModelC.py
+++ ML/ModelC.py
@@ -8,110 +8,113 @@ Created on Mon Mar  7 19:17:15 2022
 import numpy as np
 import os
 from batches_iterator import BatchesIterator
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.optimizers import Adam
-from keras.layers.normalization import BatchNormalization
-from keras.regularizers import l2
-
-from keras.callbacks import ModelCheckpoint
-
-model = Sequential()
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, BatchNormalization, Activation, Dense, Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 
-valid_lesion_dir = "Z:\\Coding\\Dataset\\Validation\\Positive"
-valid_no_lesion_dir = "Z:\\Coding\\Dataset\\Validation\\Negative"
-train_lesion_dir = "Z:\\Coding\\Dataset\\Train\\Positive"
-train_no_lesion_dir = "Z:\\Coding\\Dataset\\Train\\Negative"
+def generate_model():
+    
+    model = Sequential()
+
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(32,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(32,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Conv2D(64,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(64,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.01))
+    
+    model.add(Conv2D(64,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(64,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.01))
+    
+    model.add(Conv2D(128,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(128,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.01))
+    
+    model.add(Conv2D(256,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(256,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.01))
+    
+    model.add(Conv2D(16,3, kernel_regularizer=l2(.0001)))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.05))
+    
+    model.add(Flatten())
+    
+    model.add(Dense(1))
+    
+    model.add(Activation('sigmoid'))
+    
+    opti = Adam(lr=0.0001)
+    model.compile(loss='binary_crossentropy', optimizer=opti, metrics=[
+                      tf.keras.metrics.binary_crossentropy,
+                      'accuracy'])
+    model.build((None, 3, 224, 224))
+    model.summary()
+    
+    return model
 
 
-train_batch_size = 40000
-valid_batch_size = 50000
-
-train_batches = BatchesIterator(train_batch_size,train_no_lesion_dir,train_lesion_dir)
-valid_batches = BatchesIterator(valid_batch_size,valid_no_lesion_dir,valid_lesion_dir)
-X_valid, Y_valid = valid_batches.next()
-
-
-
-
-model.add(BatchNormalization(mode=0, axis=1, input_shape=(3,224,224)))
-model.add(Convolution2D(32,3,3,
-			 init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-
-model.add(Convolution2D(32,3,3,init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(64,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(64,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(64,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(64,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(128,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(128,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(256,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(256,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(16,3,3, init='he_normal', W_regularizer=l2(.0001)))
-model.add(BatchNormalization(mode=0, axis=1))
-model.add(Activation('relu'))
-
-model.add(Flatten())
-
-model.add(Dense(1,init='glorot_normal'))
-
-model.add(Activation('sigmoid'))
-
-opti = Adam(lr=0.0001)
-model.compile(loss='binary_crossentropy', optimizer=opti, class_mode='binary')
-model.summary()
-
-file_name = 'network_C_1'
-saveWeigts = ModelCheckpoint(file_name+'_best_weights.h5', monitor='val_acc', verbose=1, save_best_only=True)
-
-
-cllbcks= [saveWeigts]
-
-
-
-for mini_epoch in range(1000):
-	print("Epoch ", mini_epoch)
-
-	X_train, Y_train = train_batches.next()
-
-	model.fit(X_train, Y_train, batch_size=64, nb_epoch=1, callbacks = cllbcks,
-			validation_data=(X_valid,Y_valid),show_accuracy=True, verbose=1)
+if __name__=="__main__":    
+    valid_lesion_dir = "E:\\Coding\\Dataset\\Validation\\Positive"
+    valid_no_lesion_dir = "E:\\Coding\\Dataset\\Validation\\Negative"
+    train_lesion_dir = "E:\\Coding\\Dataset\\Train\\Positive"
+    train_no_lesion_dir = "E:\\Coding\\Dataset\\Train\\Negative"
+    
+    train_batch_size = 32
+    valid_batch_size = 32
+    
+    train_batches = BatchesIterator(train_batch_size,train_no_lesion_dir,train_lesion_dir)
+    valid_batches = BatchesIterator(valid_batch_size,valid_no_lesion_dir,valid_lesion_dir)
+    
+    file_name = 'network_C_1'
+    saveWeigts = ModelCheckpoint(file_name+'_best_weights.h5', monitor='accuracy', verbose=1, save_best_only=True)
+    cllbcks= [saveWeigts]
+    
+    if os.path.exists(os.path.join(os.getcwd(),file_name+'_best_weights.h5')):
+        model = tf.keras.models.load_model(file_name+'_best_weights.h5')
+    else:
+        model = generate_model()
+    
+    model.fit(x=train_batches, epochs=12, steps_per_epoch=len(train_batches), callbacks = cllbcks,
+              validation_data=valid_batches, verbose=1)
