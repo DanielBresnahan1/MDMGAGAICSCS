91e6a138eccddbd292e2de1e6a7b9ad4ea08a9ce
DanielBresnahan1 <90290413+DanielBresnahan1@users.noreply.github.com>
2022-04-06 19:51:22 -0500

    Revert "Selenium tests"

--- tests/allTestsReal.py
+++
@@ -1,8 +0,0 @@
-from randomForestTestsReal import randomForestTestsReal
-from testHomePage import testHomePage
-
-url = "https://mdmgcapstone.herokuapp.com/"
-
-
-testHomePage(url)
-randomForestTestsReal(url)
