eb4fe4ce3a53507d8e4d54d18d7920505d8990ac
DanielBresnahan1 <danielbresnahan@unomaha,edu>
2022-04-15 12:50:01 -0500

    Created Model T, the final model, for classification

--- ML/ModelT.py
+++ ML/ModelT.py
@@ -1,86 +1,132 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 14 11:14:11 2022
+Created on Tue Apr 12 19:07:30 2022
 
 @author: Danie
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  7 19:15:17 2022
-
-@author: Danie
-"""
-
-
-import tensorflow as tf
-from batches_iterator import BatchesIterator
+import numpy as np
+import os
+from test_iterator import TestIterator
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Activation, MaxPooling2D, Flatten, Dense, BatchNormalization
-from tensorflow.keras.layers import Conv2D as Convolution2D
+from tensorflow.keras.layers import Conv2D, MaxPool2D, BatchNormalization, Activation, Dense, Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+def generate_model():
+    
+    model = Sequential()
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(16,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(32,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(64,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(128,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(axis=1))
+    model.add(Conv2D(32,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.01))
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(8,3))
+    model.add(BatchNormalization(axis=1))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
 
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    for gpu in gpus:
-        tf.config.experimental.set_virtual_device_configuration(gpu,[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5120)])
-
-model = Sequential()
-
-valid_lesion_dir = "E:\\Coding\\Dataset\\Validation\\Positive"
-valid_no_lesion_dir = "E:\\Coding\\Dataset\\Validation\\Negative"
-train_lesion_dir = "E:\\Coding\\Dataset\\Train\\Positive"
-train_no_lesion_dir = "E:\\Coding\\Dataset\\Train\\Negative"
-
-train_batch_size = 200
-valid_batch_size = 500
-
-train_batches = BatchesIterator(train_batch_size,train_no_lesion_dir,train_lesion_dir)
-valid_batches = BatchesIterator(valid_batch_size,valid_no_lesion_dir,valid_lesion_dir)
-
-
-model.add(BatchNormalization(axis=1, input_shape=(3,224,224)))
-model.add(Convolution2D(16,3,3, data_format="channels_first"))
-model.add(BatchNormalization(axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-
-model.add(Convolution2D(8,3,3, data_format="channels_first"))
-model.add(BatchNormalization(axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Convolution2D(8,3,3, data_format="channels_first"))
-model.add(BatchNormalization(axis=1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
-
-model.add(BatchNormalization())
-model.add(Dense(1))
-
-model.add(Activation('sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='rmsprop',metrics=[
-                  tf.keras.losses.BinaryCrossentropy(name='binary_crossentropy'),
-                  'accuracy'])
-model.summary()  
-
-file_name = 'network_T_1'
-saveWeigts = ModelCheckpoint(file_name+'_best_weights.h5', monitor='accuracy', verbose=1, save_best_only=True)
-
-cllbcks= [saveWeigts]
-
+    model.add(Flatten())
+    
+    model.add(BatchNormalization())
+    model.add(Dense(1))
+    
+    opti=Adam(lr=0.0001)
+    model.compile(loss='binary_crossentropy', optimizer=opti, metrics=[
+                      tf.keras.metrics.binary_crossentropy,
+                      'accuracy'])
+    
+    model.build((None, 1, 193, 126))
+    model.summary()
+    
+    return model
 
 if __name__=="__main__":
     
-    for mini_epoch in range(1000):
-        print("Epoch ", mini_epoch)
     
-        X_train, Y_train, _ = next(train_batches)
-        X_valid, Y_valid, _ = next(valid_batches)
+    base_dir = "E:\\Coding\\Dataset"
+    train_image_dir = os.path.join(base_dir, "Test_Map_Heat")
+    val_image_dir = os.path.join(base_dir, "Train_Map_Heat")
+    batch_size = 32
+    train_csvf = "test_labels.csv"
+    val_csvf = "train_map.csv"
     
-        model.fit(X_train, Y_train, callbacks = cllbcks,
-    			validation_data=(X_valid,Y_valid), verbose=0)
+    epochsN = 128
+    
+    train_csv_dir = os.path.join(base_dir, train_csvf)
+    val_csv_dir = os.path.join(base_dir, val_csvf)
+    
+    train_batches = TestIterator(batch_size, train_csv_dir, train_image_dir)
+    val_batches = TestIterator(batch_size, val_csv_dir, val_image_dir)
+    
+    file_name = 'network_T_1'
+    saveWeigts = ModelCheckpoint(file_name+'_best_weights.h5', monitor='accuracy', verbose=1, save_best_only=True)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(os.getcwd(), "log"), histogram_freq=1)
+    
+    cllbcks= [saveWeigts, tensorboard_callback]
+    
+    model_path = os.path.join(os.getcwd(),file_name+'_best_weights.h5')
+    
+    if os.path.exists(model_path):
+        model = tf.keras.models.load_model(file_name+'_best_weights.h5')
+    else:
+        model = generate_model()
+    
+    model.fit(x=train_batches, epochs=epochsN, steps_per_epoch=len(train_batches), callbacks = cllbcks,
+              validation_data=val_batches, verbose=1)
+    
+    model.save(model_path, include_optimizer=False)
--- ML/test_iterator.py
+++ ML/test_iterator.py
@@ -10,23 +10,44 @@ import numpy as np
 import os
 import PIL
 import math
+import csv
+import random
 
+"""@package
+Keras Sequence Iterator for heat map images, for training the final model
+"""
 class TestIterator(Sequence):
-    def __init__(self, batch_size, label,
-              image_folder_path, return_name=True):
+    def __init__(self, batch_size, csv_dir,
+              image_folder_path, return_name=False, preserve_image=False):
 
         self.batch_size = batch_size
         self.files = []
-        self.label = label
+        self.labels = {}
+        self.csv_dir = csv_dir
         self.return_name = return_name
-        for file in os.listdir(image_folder_path):
-            self.files.append((os.path.join(image_folder_path, file), label))
+        self.preserve_image = preserve_image
+        self.get_labels()
+        for folder in os.listdir(image_folder_path):
+            for file in os.listdir(os.path.join(image_folder_path, folder)):
+                self.files.append((os.path.join(image_folder_path, folder, file), self.labels[folder]))
 
             
     
     def __len__(self):
         return math.ceil(len(self.files)/self.batch_size)
     
+
+    def get_labels(self):
+        
+        with open(self.csv_dir, "r") as f:
+            reader = csv.reader(f)
+            
+            for row in reader:
+                self.labels.update({row[0].split(".")[0]:int(row[1])})
+        
+    def on_epoch_end(self):
+        if not self.preserve_image:
+            random.shuffle(self.files)
 
     def __getitem__(self, index):
 
@@ -35,41 +56,45 @@ class TestIterator(Sequence):
         else:
             this_batch_size = self.batch_size
 
-        batch_x = np.zeros((this_batch_size,3,224,224), dtype='uint8')
+        batch_x = np.zeros((this_batch_size,1,193,126))
 		
         batch_y = np.zeros((this_batch_size), dtype='uint8')
 
         batch_file_names = []
         for x in range(this_batch_size):
-            this_file_name = self.files[(index*self.batch_size)+x][0]
+            this_file_name, this_file_label = self.files[(index*self.batch_size)+x]
+            # print(this_file_name)
             pic = PIL.Image.open(this_file_name)
+            # print("Image_Shape: {}".format(pic.size))
+            this_pic = np.zeros((193, 126))
             pic = np.array(pic)
-            batch_x[x]=pic.reshape(3,224,224)
-            batch_y[x]=self.label
+            # print("Numpy array shape: {}".format(pic.shape))
+            
+            this_pic[:,:] = pic
+            
+            batch_x[x]=this_pic
+            batch_y[x]=this_file_label
             batch_file_names.append(this_file_name) 
             
         if self.return_name:
             return batch_x, batch_y, batch_file_names
         else:
             return batch_x, batch_y
-    
 
 
 if __name__=="__main__":
     
     
-    test_image = "DSC00027"
     base_dir = "E:\\Coding\\Dataset"
-    image_dir = os.path.join(base_dir, "Train_Map", test_image)
-    batch_size = 32
+    image_dir = os.path.join(base_dir, "Train_Map_Heat")
+    batch_size = 8
     csvf = "train_map.csv"
-    label = 0
+    csv_dir = os.path.join(base_dir, csvf)
     
+    testIter = TestIterator(batch_size, csv_dir, image_dir, return_name=True)
     
-    testIter = TestIterator(batch_size, label, image_dir, return_name=True)
-    
-    for i in range(len(testIter)):
+    for i in range(2):
         x, y, name = testIter.__getitem__(i)
-        print("File: {}, Label: {}".format(name, y))
+        print(x)
     
     
