5484e2941faeca18980e9f40265e8c3b47ad224c
DanielBresnahan1 <danielbresnahan@unomaha,edu>
2022-04-27 20:07:36 -0500

    Changes to git ignore, ignore .bak

--- ML/colate_data.py
+++ ML/colate_data.py
@@ -75,8 +75,8 @@ def patch_all(base_dir: str, annotations_csv: str, pic_folder: str, save_folder:
 
 if __name__=="__main__":
     base_dir = "E:\Coding\Dataset"
-    annotations_csv="annotations_val.csv"
-    pic_folder="images_validation"
-    save_folder="Validation"
+    annotations_csv="annotations_train.csv"
+    pic_folder="images_handheld"
+    save_folder="Train"
     patch_all(base_dir, annotations_csv, pic_folder, save_folder)
         
